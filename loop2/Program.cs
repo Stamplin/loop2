e226dab2a992128ba598f911c6a73511264877ab
Sakarona Bora <701017313@GoTVDSB.ca>
2024-09-25 09:09:54 -0400

    Add project files.

---
+++ loop2/Program.cs
@@ -0,0 +1,39 @@
+﻿//task1
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i + ". Icecream");
+}
+
+//task2
+
+int countTothis;
+Console.WriteLine("what do you want me to count to?");
+int.TryParse(Console.ReadLine(), out countTothis);
+for (int i = 0; i <= countTothis; i++)
+{
+    Console.WriteLine(i);
+}
+
+//task 3
+
+int countFrom, countTo, countBy;
+Console.WriteLine("input the number you want to count to: ");
+int.TryParse(Console.ReadLine(), out countTo);
+Console.WriteLine("input the number you want to count from: ");
+int.TryParse(Console.ReadLine(), out countFrom);
+Console.WriteLine("input the number you want to count by: ");
+int.TryParse(Console.ReadLine(), out countBy);
+
+if (countBy > 0)
+{
+    for (int i = countTo; i <= countFrom; i += countBy)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+//Programming Assignments
+
+//task1 countingdown
+
