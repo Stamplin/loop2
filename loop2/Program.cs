8226a7c86a289beb8c56bbdf853d9ce45694e199
Stamplin <701017313@gotvdsb.ca>
2024-09-26 20:29:19 -0700

    Update Program.cs

--- loop2/Program.cs
+++ loop2/Program.cs
@@ -1,78 +1,73 @@
-////task1
+//task1
 
-//for (int i = 0; i <= 10; i++)
-//{
-//    Console.WriteLine(i + ". Icecream");
-//}
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i + ". Icecream");
+}
 
-////task2
+//task2
 
-//int countTothis;
-//Console.WriteLine("what do you want me to count to?");
-//int.TryParse(Console.ReadLine(), out countTothis);
-//for (int i = 0; i <= countTothis; i++)
-//{
-//    Console.WriteLine(i);
-//}
+int countTothis;
+Console.WriteLine("what do you want me to count to?");
+int.TryParse(Console.ReadLine(), out countTothis);
+for (int i = 0; i <= countTothis; i++)
+{
+    Console.WriteLine(i);
+}
 
-////task 3
+//task 3
 
-//int countFrom, countTo, countBy;
-//Console.WriteLine("input the number you want to count to: ");
-//int.TryParse(Console.ReadLine(), out countTo);
-//Console.WriteLine("input the number you want to count from: ");
-//int.TryParse(Console.ReadLine(), out countFrom);
-//Console.WriteLine("input the number you want to count by: ");
-//int.TryParse(Console.ReadLine(), out countBy);
+int countFrom, countTo, countBy;
+Console.WriteLine("input the number you want to count to: ");
+int.TryParse(Console.ReadLine(), out countTo);
+Console.WriteLine("input the number you want to count from: ");
+int.TryParse(Console.ReadLine(), out countFrom);
+Console.WriteLine("input the number you want to count by: ");
+int.TryParse(Console.ReadLine(), out countBy);
 
-//if (countBy > 0)
-//{
-//    for (int i = countTo; i <= countFrom; i += countBy)
-//    {
-//        Console.WriteLine(i);
-//    }
-//}
+if (countBy > 0)
+{
+    for (int i = countTo; i <= countFrom; i += countBy)
+    {
+        Console.WriteLine(i);
+    }
+}
 
-////Programming Assignments
+//Programming Assignments
 
-////task1-countingdown
+//task1-countingdown
 
-//for (int i = 10; i >= 0; i--)
-//{
-//    Console.WriteLine(i);
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
 
-//}
-//Console.WriteLine("Blast off!");
+}
+Console.WriteLine("Blast off!");
 
 
-////task2-xandy
+//task2-xandy
 
-//Console.WriteLine("X" + "\t" + "Y");
-//for (int i = -10; i <= 10; i = i + 2)
-//{
-//    int j = i * i;
-//    Console.WriteLine(i + "\t" + j);
-//}
+Console.WriteLine("X" + "\t" + "Y");
+for (int i = -10; i <= 10; i = i + 2)
+{
+    int j = i * i;
+    Console.WriteLine(i + "\t" + j);
+}
 
 //task3-loopsforfun
-string userInput;
 
+using System;
+
+string userInput;
+int repeatCount = 10;
 Console.WriteLine("please input a name:");
 userInput = Console.ReadLine().ToUpper();
 
 if (userInput == "ALDWORTH")
 {
-  
-
+    repeatCount = 5;
 }
-
-// for (int i = 0; i < 5; i++)
-//{
-//    Console.WriteLine("Aldworth");
-//}
-
-
-//for (int i = 0; i < 10; i++)
-//{
-//    Console.WriteLine(userInput);
-//}
+for (int i = 0; i < repeatCount; i++)
+{
+    Console.WriteLine(userInput);
+}
